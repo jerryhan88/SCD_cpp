e96110bf2dcd9ba9d242651e3d3e80c6a09ed072
Chung-Kyun HAN <chungkyun.han@gmail.com>
2020-02-16 10:44:40 +0800

    Initial Commit

---
+++ SCD_cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SCD_cpp
+//
+//  Created by Chung-Kyun HAN on 16/2/20.
+//  Copyright © 2020 Chung-Kyun HAN. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
